5f1737550b5d6324cbad1e6c4d125e4442aa2e41
Jin Peng <jpeng124@terpmail.umd.edu>
2026-05-17 18:16:45 -0400

    node files

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+
+
+app.use(express.static(__dirname + '/public'));
+
+
+
+app.get('/', (req, res) => {
+  res.sendFile('public/home.html', { root: __dirname });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`App is available on port: ${port}`);
+});
