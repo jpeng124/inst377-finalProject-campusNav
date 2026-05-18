291c09f986b8392ef4414dfa86079ccd83c9af60
Jin Peng <jpeng124@terpmail.umd.edu>
2026-05-17 23:31:11 -0400

    added env

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+
 const port = 3001;
 
 const supabaseClient = require("@supabase/supabase-js");
@@ -33,9 +34,40 @@ app.get("/umdbuilding", async (req, res) => {
 
 // Post
 app.post("/umdbuilding", async (req, res) => {
-  console.log("Adding New Building Name");
+  console.log("Adding UMD Building");
   console.log(`Request: ${JSON.stringify(req.body)}`);
-  res.json({});
+
+  const abbreviation = req.body.abbreviation;
+  const fullName = req.body.full_name;
+  const address = req.body.address;
+  const mapUrl = req.body.map_url;
+  
+
+  if (!abbreviation || !fullName || !address || !mapUrl) {
+    res.statusCode = 400;
+    res.json({
+      message: "abbreviation, full_name, address, and map_url are required",
+    });
+    return;
+  }
+
+  const { data, error } = await supabase
+    .from("UMDbuildings")
+    .insert({
+      abbreviation: abbreviation,
+      full_name: fullName,
+      address: address,
+      map_url: mapUrl,
+    })
+    .select();
+
+  if (error) {
+    console.log(`Error: ${error}`);
+    res.statusCode = 500;
+    res.send(error);
+  } else {
+    res.json(data);
+  }
 });
 
 app.listen(port, () => {
